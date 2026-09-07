d1511730a2f1359ff128b8ed48b50a6f8a7eb642
Tanvir ahmed <tanvirahmed1294@gmail.com>
2026-09-07 17:26:52 +0600

    Configure React + Tailwind CSS + GSAP with working build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 import { gsap } from 'gsap';
-import { useGSAP } from 'gsap/React';
+import { useGSAP } from '@gsap/react';
 
 function App() {
   const container = useRef<HTMLDivElement>(null);
